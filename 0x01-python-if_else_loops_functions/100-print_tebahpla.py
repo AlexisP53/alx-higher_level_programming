11a831b41dd5eb49391d4e47cd280df5825fdb82
alexis <alexisalezandre53@gmail.com>
2023-11-28 07:24:26 -0800

    Python - if/else

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    char = i
-    if i % 2 != 0:
-        char = char - 32
-    print("{:c}".format(char), end="")
+for c in range(ord('z'), ord('a') - 1, -2):
+    print("{:c}{:s}".format(c, chr(c - 33)), end="")
